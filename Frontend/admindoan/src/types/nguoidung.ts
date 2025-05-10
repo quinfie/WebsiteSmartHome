d36d04ceba79ece5c8878af578afe3f11a260aa2
Mibions <nlbthanh@gmail.com>
2025-05-10 17:39:38 +0700

    Update

--- Frontend/admindoan/src/types/nguoidung.ts
+++ Frontend/admindoan/src/types/nguoidung.ts
@@ -1,29 +1,32 @@
 export interface NguoiDungDto {
-    id: string;
-    tenNguoiDung: string;
-    gioiTinh: string;
-    ngaySinh: string | null;
-    cccd: string;
-    sdt: string;
-    diaChi: string;
-  }
-  
-  export interface NguoiDungCreateDto {
-    tenNguoiDung: string;
-    gioiTinh: string;
-    ngaySinh: string | null;
-    cccd: string;
-    sdt: string;
-    diaChi: string;
-    maVaiTro: string;
-  }
-  
-  export interface NguoiDungUpdateDto {
-    tenNguoiDung?: string;
-    gioiTinh?: string;
-    ngaySinh?: string | null;
-    diaChi?: string;
-    cccd?: string;
-    sdt?: string;
-  }
-  
+  id: string;
+  maNguoiDung?: string;
+  tenNguoiDung: string;
+  gioiTinh: string;
+  ngaySinh: Date;
+  cccd: string;
+  sdt: string;
+  diaChi: string;
+  maTaiKhoan?: string;
+  maVaiTro?: string;
+}
+
+export interface NguoiDungUpdateDto {
+  tenNguoiDung: string;
+  gioiTinh: string;
+  ngaySinh: Date;
+  cccd: string;
+  sdt: string;
+  diaChi: string;
+}
+
+export interface NguoiDungCreateDto {
+  tenNguoiDung: string;
+  gioiTinh: string;
+  ngaySinh: Date;
+  cccd: string;
+  sdt: string;
+  diaChi: string;
+  maTaiKhoan: string;
+  tenVaiTro: string;
+} 
